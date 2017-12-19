54b6de527e18b2706263ff3e431374d5d653b05b
iliasvw <iliasvw@hotmail.com>
2017-12-19 22:18:37 +0100

    Add LoginActivity

--- app/src/main/java/com/example/eternal/eternalapp/LoginActivity.java
+++ app/src/main/java/com/example/eternal/eternalapp/LoginActivity.java
@@ -2,12 +2,43 @@ package com.example.eternal.eternalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.usernameText)
+    EditText usernameText;
+
+    @BindView(R.id.passwordText)
+    EditText passwordText;
+
+    @BindView(R.id.login)
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        ButterKnife.bind(this);
+
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if(!usernameText.getText().toString().equals("") || !passwordText.getText().toString().equals(""))
+                    Log.i("LoginActivity", " ### Login clicked! ###");
+                else {
+                    Toast.makeText(LoginActivity.this, "Invalid login", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
